0629e85570cca2e16082ae4eb6b1069a1d33a8bf
abhishek7171 <32305191+abhishek7171@users.noreply.github.com>
2017-09-26 22:07:32 +0530

    Add files via upload

---
+++ cbir.py
@@ -0,0 +1,124 @@
+import os,cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.utils import shuffle
+from sklearn.cross_validation import train_test_split
+
+import tensorflow
+
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD,RMSprop,adam
+
+#%%
+
+PATH = os.getcwd()
+# Define data path
+data_path = PATH + '/data'
+data_dir_list = os.listdir(data_path)
+
+img_rows=128
+img_cols=128
+num_channel=1
+num_epoch=20
+
+# Define the number of classes
+#num_classes = 10
+
+img_data_list=[]
+
+for dataset in data_dir_list:
+	img_list=os.listdir(data_path+'/'+ dataset)
+	print ('Loaded the images of dataset-'+'{}\n'.format(dataset))
+	for img in img_list:
+		input_img=cv2.imread(data_path + '/'+ dataset + '/'+ img )
+		input_img=cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
+		input_img_resize=cv2.resize(input_img,(128,128))
+		img_data_list.append(input_img_resize)
+
+img_data = np.array(img_data_list)
+img_data = img_data.astype('float32')
+img_data /= 255
+print (img_data.shape)
+img_data= np.expand_dims(img_data, axis=4) 
+print (img_data.shape)
+num_classes=10
+num_of_samples = img_data.shape[0]
+label = np.ones((num_of_samples,),dtype='int64')
+label[0:100]=0
+label[100:200]=1
+label[200:300]=2
+label[300:400]=3
+label[400:500]=4
+label[500:600]=5
+label[600:700]=6
+label[700:800]=7
+label[800:900]=8
+label[900:1000]=9
+# convert class labels to on-hot encoding
+Y = np_utils.to_categorical(labels, num_classes)
+
+#Shuffle the dataset
+x,y = shuffle(img_data,Y, random_state=2)
+# Split the dataset
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
+
+#%%
+# Defining the model
+input_shape=img_data[0].shape
+					
+model = Sequential()
+
+model.add(Convolution2D(32, 3,3,border_mode='same',input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(Convolution2D(32, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Convolution2D(64, 3, 3))
+model.add(Activation('relu'))
+#model.add(Convolution2D(64, 3, 3))
+#model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+#sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+#model.compile(loss='categorical_crossentropy', optimizer=sgd,metrics=["accuracy"])
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop',metrics=["accuracy"])
+#  model_configuration
+
+model.summary()
+model.get_config()
+model.layers[0].get_config()
+model.layers[0].input_shape			
+model.layers[0].output_shape			
+model.layers[0].get_weights()
+np.shape(model.layers[0].get_weights()[0])
+model.layers[0].trainable
+# Training
+hist = model.fit(X_train, y_train, batch_size=32, nb_epoch=num_epoch, verbose=1, validation_data=(X_test, y_test))
+
+#hist = model.fit(X_train, y_train, batch_size=32, nb_epoch=20,verbose=1, validation_split=0.2)
+
+  #Evaluating the model
+score = model.evaluate(X_test, y_test, show_accuracy=True, verbose=0)
+print('Test Loss:', score[0])
+print('Test accuracy:', score[1])
+
+test_image = X_test[0:1]
+print (test_image.shape)
+
+print(model.predict(test_image))
+print(model.predict_classes(test_image))
+print(y_test[0:1])
